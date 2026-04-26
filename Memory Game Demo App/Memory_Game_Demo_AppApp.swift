638bff7463d1c3138813d28022a2c7edc28bfded
miroszlav <miroszlav.jani@swisscom.com>
2026-04-26 22:34:36 +0200

    Initial Commit

---
+++ Memory Game Demo App/Memory_Game_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_Game_Demo_AppApp.swift
+//  Memory Game Demo App
+//
+//  Created by Miroszláv Jani on 26.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Memory_Game_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memory Game Demo AppTests/Memory_Game_Demo_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_Game_Demo_AppTests.swift
+//  Memory Game Demo AppTests
+//
+//  Created by Miroszláv Jani on 26.04.2026.
+//
+
+import Testing
+@testable import Memory_Game_Demo_App
+
+struct Memory_Game_Demo_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
